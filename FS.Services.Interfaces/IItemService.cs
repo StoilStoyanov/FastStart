b3120c3ffaa5ec412d517e7a9c6897d019c3fedd
Stoil.Stoyanov <stoyanov.stoil@gmail.com>
2018-07-30 16:59:21 +0300

    Initial services and service interfaces

---
+++ FS.Services.Interfaces/IItemService.cs
@@ -0,0 +1,12 @@
+﻿using FS.DTOs;
+using System.Collections.Generic;
+
+namespace FS.Services.Interfaces
+{
+    public interface IItemService
+    {
+        List<ItemDTO> GetAll();
+
+        ItemDTO GetById(int id);
+    }
+}
---
+++ FS.Services/ItemService.cs
@@ -0,0 +1,38 @@
+﻿using FS.DTOs;
+using FS.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FS.Services
+{
+    public class ItemService : IItemService
+    {
+        private List<ItemDTO> AllItems { get; set; }
+
+        public ItemService()
+        {
+            AllItems = new List<ItemDTO>()
+            {
+                new ItemDTO() { Id = 1, Name ="Alfa" },
+                new ItemDTO() { Id = 2, Name ="Bravo" },
+                new ItemDTO() { Id = 3, Name ="Charlie" }
+            };
+        }
+
+        public List<ItemDTO> GetAll()
+        {
+            return this.AllItems;
+        }
+        public ItemDTO GetById(int id)
+        {
+            var result = this.AllItems.FirstOrDefault(x => x.Id == id);
+            if (result == null)
+            {
+                throw new Exception($"Item with id {id} not found");
+            }
+
+            return result;
+        }
+    }
+}
