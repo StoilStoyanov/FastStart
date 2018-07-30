4c76be561dcb94f1b1651ab9a09d8235f8a9b8b3
Stoil.Stoyanov <stoyanov.stoil@gmail.com>
2018-07-30 16:41:32 +0300

    Entities and DTOs projects

---
+++ FS.DTOs/ItemDTO.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace FS.DTOs
+{
+    public class ItemDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public static Expression<Func<Item, ItemDTO>> ProjectTo()
+        {
+            return x => new ItemDTO
+            {
+                Id = x.Id,
+                Name = x.Name
+            };
+        }
+    }
+}
